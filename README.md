1f202da61bca31ab772a44b3c8e7f62662a74817
JESSE-star-byte <mathias.blomster@gmail.com>
2022-05-22 16:41:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Random password generator.
+
+html + css for webpage
+javascript for generating random strong passwords.
+
+password checker, practice regexpressions.
---
+++ generator.js
@@ -0,0 +1,11 @@
+function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxzy!#¤%&/()ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    var passwordLength = document.getElementById("length").value;
+    var password="";
+
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById("password").value = password;
+}
